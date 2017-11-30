c78e336b86f253aaf15b4d9d4eceb714f1fd626d
Guest User <Guest@Epicodus-8B.local>
2017-11-29 19:35:44 -0800

    Alter code to call for properties of 'Pizza' object and append them to the appropriate spans

--- js/scripts.js
+++ js/scripts.js
@@ -11,18 +11,18 @@ $(document).ready(function(){
   $("form#pizza_survey").submit(function(event){
     event.preventDefault();
     var sizeMode = $("input:radio[name=size]:checked").val();
-    $('#output').append(sizeMode + "<br>");
     var pizzaColor = $("#color").val();
     $('#output').append(pizzaColor + "<br>");
     var pizzaStyle = $("input:radio[name=style]:checked").val();
-    $('#output').append(pizzaStyle + "<br>");
     $("input:checkbox[name=toppings]:checked").each(function(){
       var toppingsMode = $(this).val();
       toppingsArray.push(toppingsMode);
     });
-    $('#output').append(toppingsArray + "<br>");
     var newPizza = new Pizza(toppingsArray, sizeMode, pizzaStyle, pizzaColor);
     console.log(newPizza);
+    $('#topp-output').append(newPizza.toppings + "<br>");
+    $('#size-output').append(newPizza.size + "<br>");
+    $('#style-output').append(newPizza.style + "<br>");
     $("#output").show();
   });
 });
