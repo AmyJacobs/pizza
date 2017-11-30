70784f68267b9ed92a64dbbfebed47560bcb0e39
Guest User <Guest@Epicodus-8B.local>
2017-11-29 19:02:52 -0800

    Create basic system of questions and code to collect & display answers

--- js/scripts.js
+++ js/scripts.js
@@ -1,11 +1,16 @@
 $(document).ready(function(){
   $("form#pizza_survey").submit(function(event){
     event.preventDefault();
-    $("#output").show();
+    var sizeMode = $("input:radio[name=size]:checked").val();
+    $('#output').append(sizeMode + "<br>");
+    var pizzaColor = $("#color").val();
+    $('#output').append(pizzaColor + "<br>");
+    var pizzaStyle = $("input:radio[name=style]:checked").val();
+    $('#output').append(pizzaStyle + "<br>");
     $("input:checkbox[name=toppings]:checked").each(function(){
       var toppingsMode = $(this).val();
       $('#output').append(toppingsMode + "<br>");
     });
-
+    $("#output").show();
   });
 });
